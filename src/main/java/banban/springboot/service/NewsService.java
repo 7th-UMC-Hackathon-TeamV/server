8a27d85a255f61d16bb2fe4733716c836f4ae3bb
dokyung-kang <kangdk02@naver.com>
2025-01-11 22:48:32 +0900

    feat : 공감 누르기 기능

--- src/main/java/banban/springboot/service/NewsService.java
+++ src/main/java/banban/springboot/service/NewsService.java
@@ -1,5 +1,8 @@
 package banban.springboot.service;
 
+import banban.springboot.apiPayload.code.status.ErrorStatus;
+import banban.springboot.apiPayload.exception.handler.NewsHandler;
+import banban.springboot.domain.entity.News;
 import banban.springboot.repository.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +13,15 @@ public class NewsService {
     @Autowired
     private NewsRepository newsRepository;
 
+    // 뉴스 공감 누르기
+    public News addNewsLike(Long newsId){
+        News existNews = newsRepository.findById(newsId)
+                .orElseThrow(() -> new NewsHandler(ErrorStatus.NEWS_NOT_EXIST_FOUND));
+
+        Integer likes_count = existNews.getLikes() + 1;
+
+        existNews.setLikes(likes_count);
+
+        return newsRepository.save(existNews);
+    }
 }
