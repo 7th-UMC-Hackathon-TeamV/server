2be8aba5e67b0a22598777c75c341366ccba3625
bikooju <jhjk1234@gmail.com>
2025-01-12 00:41:05 +0900

    로그인 구현

--- src/main/java/banban/springboot/repository/MemberRepository.java
+++ src/main/java/banban/springboot/repository/MemberRepository.java
@@ -1,14 +1,12 @@
 package banban.springboot.repository;
 
 import banban.springboot.domain.entity.Member;
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByUsername(String userId);
+    Optional<Member> findByUsername(String username);
 }
