c9f8f6c4da94ed5c1d78be50d6e75f8d1ed15a9f
bikooju <jhjk1234@gmail.com>
2025-01-12 07:45:52 +0900

    최종

--- src/main/java/banban/springboot/config/AmazonConfig.java
+++ src/main/java/banban/springboot/config/AmazonConfig.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Getter
-public class AmazonConfig {
+public class    AmazonConfig {
 
     private AWSCredentials awsCredentials;
 
